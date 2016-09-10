257a501d33db090172bc43ecac276bfdd6e58504
Doug Henderson <djhenderson@users.noreply.github.com>
2016-09-09 23:05:38 -0600

    clean up setup.py and add a classifier

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ if sys.version_info < (3, 0):
 
 
 setup(name='picotui',
-      version='0.8.1',
-      description="""A simple text user interface (TUI) library.""",
-      long_description=open('README.rst').read(),
-      url='https://github.com/pfalcon/picotui',
-      author='Paul Sokolovsky',
-      author_email='pfalcon@users.sourceforge.net',
-      license='MIT',
-      packages=['picotui'])
+    version='0.8.1',
+    description="""A simple text user interface (TUI) library.""",
+    long_description=open('README.rst').read(),
+    url='https://github.com/pfalcon/picotui',
+    author='Paul Sokolovsky',
+    author_email='pfalcon@users.sourceforge.net',
+    license='MIT',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        ],
+    packages=['picotui'])
